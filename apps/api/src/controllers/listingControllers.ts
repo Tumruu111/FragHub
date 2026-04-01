0dcf7e67140bd61541a6163507a4a7ec4ba2abae
BACK-2-15 <back-2-15@Erxess-Mac-mini.local>
2026-04-01 20:27:40 +0800

    login register middleware hiih

--- apps/api/src/controllers/listingControllers.ts
+++ apps/api/src/controllers/listingControllers.ts
@@ -2,16 +2,28 @@ import { Request, Response } from 'express';
 import { prisma } from '../lib/prisma';
 
 export const createList = async (req: Request, res: Response) => {
-  const { title, vibe, status, size, picture } = req.body;
-  const list = await prisma.listing.create({
-    data: {
-      title,
-      vibe,
-      status,
-      size,
-      picture,
-    },
-  });
-  res.send({ message: 'List created successfully' });
-  return list;
+  try {
+    const { title, vibe, status, size, picture } = req.body;
+
+    const list = await prisma.listing.create({
+      data: {
+        title,
+        vibe,
+        status,
+        size,
+        picture,
+      },
+    });
+
+    return res.send({
+      message: 'List created successfully',
+      data: list,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(400).send({
+      message: 'Failed to create listing',
+      error,
+    });
+  }
 };
